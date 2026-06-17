a7885538cca19274239860d9e51ab7034e8fcf9e
Tom <127806801+dd-ray@users.noreply.github.com>
2026-06-17 13:07:38 +0800

    更新 tool.sh

--- script/tool.sh
+++ script/tool.sh
@@ -45,14 +45,14 @@ function build_config() {
   cat "${BUILDER_PATH}/config/common.seed" >> .config
   echo -e 'CONFIG_DEVEL=y' >> .config
   echo -e 'CONFIG_CCACHE=y' >> .config
-
+  echo 'CONFIG_LINUX_6_12=y' >> .config
   if [ "$REPO_BRANCH" == "openwrt-24.10" ]; then
     echo "OpenWrt 24.10"
     echo 'CONFIG_LINUX_6_6=y' >> .config
   fi
   if [ "$REPO_BRANCH" == "main" ]; then
     echo "OpenWrt SNAPSHOT"
-    echo 'CONFIG_LINUX_6_12=y' >> .config
+    echo 'CONFIG_LINUX_6_18=y' >> .config
   fi
 
   if [ -f "${BUILDER_PATH}/script/diy.sh" ]; then
